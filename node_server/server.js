1812e1f856b62ad53c36a49a432a9752ec56c4d3
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-02-10 13:43:24 -0800

    more comments, clearer functionality

--- node_server/server.js
+++ node_server/server.js
@@ -5,8 +5,10 @@ const cfg = require('./server_settings.js');
 var fs = require('fs');
 var path = require('path');
 
+//when a request is made to the server from a user, what should we do with it?
 function onRequest(request, response) {
     console.log('request ', request.url);
+    //console.log(request); //debug
 
     //process the request, in this case simply reading a file based on the request url    
     var filePath = '.' + request.url;
@@ -15,17 +17,20 @@ function onRequest(request, response) {
         filePath = cfg.settings.startpage; //point to the start page
     }
 
+    //read the file on the server
     fs.readFile(filePath, function(error, content) {
         if (error) {
             if(error.code == 'ENOENT') { //page not found: 404
                 fs.readFile(cfg.settings.errpage, function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
+                    response.writeHead(404, { 'Content-Type': 'text/html' }); //set response headers
+                    response.end(content, 'utf-8'); //set response content
+
+                    //console.log(content); //debug
                 });
             }
             else { //other error
-                response.writeHead(500);
-                response.end('Something went wrong: '+error.code+' ..\n');
+                response.writeHead(500); //set response headers
+                response.end('Something went wrong: '+error.code+' ..\n'); //set response content
             }
         }
         else { //file read successfully, serve the content back
@@ -33,37 +38,35 @@ function onRequest(request, response) {
             //set content type based on file path extension for the browser to read it properly
             var extname = String(path.extname(filePath)).toLowerCase();
             var mimeTypes = {
-                '.html': 'text/html',
-                '.js': 'text/javascript',
-                '.css': 'text/css',
-                '.json': 'application/json',
-                '.png': 'image/png',
-                '.jpg': 'image/jpg',
-                '.gif': 'image/gif',
-                '.svg': 'image/svg+xml',
-                '.wav': 'audio/wav',
-                '.mp4': 'video/mp4',
-                '.woff': 'application/font-woff',
-                '.ttf': 'application/font-ttf',
-                '.eot': 'application/vnd.ms-fontobject',
-                '.otf': 'application/font-otf',
+                '.html': 'text/html', '.js': 'text/javascript', '.css': 'text/css', '.json': 'application/json',
+                '.png': 'image/png', '.jpg': 'image/jpg', '.gif': 'image/gif', '.svg': 'image/svg+xml',
+                '.wav': 'audio/wav', '.mp4': 'video/mp4',
+                '.woff': 'application/font-woff', '.ttf': 'application/font-ttf', '.eot': 'application/vnd.ms-fontobject', '.otf': 'application/font-otf',
                 '.wasm': 'application/wasm'
             };
 
             var contentType = mimeTypes[extname] || 'application/octet-stream';
 
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
+            response.writeHead(200, { 'Content-Type': contentType }); //set response headers
+            response.end(content, 'utf-8'); //set response content
+
+            //console.log(content); //debug
         }
     });
+
+    //console.log(response); //debug
 }
 
+
+//runs when the server starts successfully.
 function onStarted() {      
     console.log(`Server running at 
         ${cfg.settings.protocol}://${cfg.settings.host}:${cfg.settings.port}/`
     );
 }
 
+
+//now create the http/https server. For hosted servers, use the IP and open ports. Default html port is 80 or sometimes 443
 if(cfg.settings.protocol === 'http') {
     
     var http = require('http');
